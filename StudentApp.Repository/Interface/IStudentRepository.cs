51661d65f37cb526ccdf224fc8e6f930f69401ac
Santosh Yadav <santosh.yadav198613@gmail.com>
2016-07-23 14:28:03 +0530

    Changes for DI using unity

---
+++ StudentApp.Repository/Interface/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudentApp.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentApp.Repository.Interface
+{
+    public interface IStudentRepository
+    {
+         Task<List<Student>> Get();
+         Task<bool> Add(Student student);
+
+    }
+}
---
+++ StudentApp.Repository/StudentRepository.cs
@@ -0,0 +1,32 @@
+﻿using StudentApp.Model;
+using StudentApp.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace StudentApp.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private StudentContext _context = new StudentContext();
+
+        public StudentRepository(StudentContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<List<Student>> Get()
+        {
+            return await _context.Student.ToListAsync();
+        }
+
+        public async Task<bool> Add(Student student)
+        {
+            student.CreatedDate = DateTime.Now;
+            _context.Student.Add(student);
+            int x = await _context.SaveChangesAsync();
+            return x == 0 ? false : true;
+        }
+    }
+}
